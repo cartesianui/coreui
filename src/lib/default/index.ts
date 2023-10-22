8872f992a083e457c6bcd8a61c477cf23abf18bd
Syed Ali Murtaza <ali@kazmi.me>
2023-10-22 17:15:13 +0500

    removes default action move into common module as widget

--- src/lib/default/index.ts
+++ src/lib/default/index.ts
@@ -1,6 +1,5 @@
 export * from './default-footer/default-footer.component';
 export * from './default-header/default-header.component';
 export * from './default-page-title/default-page-title.component';
-export * from './default-actions/default-actions.component';
 export * from './offcanvas/offcanvas.component';
 export * from './default-layout.component';
